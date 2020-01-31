83ccf90d8d188681d061d692f727eeb69afce1fe
Polina <49366794+PolinaRaikina@users.noreply.github.com>
2020-01-31 17:43:43 +0300

    no id changes

--- script.js
+++ script.js
@@ -1,32 +1,29 @@
-document.getElementById('main_content__add').onclick = function (){
-
-    var todoList = document.getElementById("main_content__block"); 
+var add = document.querySelector('.main_content__add');
+var clear = document.querySelector('.main_content__clear');
+var todoList = document.querySelector(".main_content_block"); 
+add.addEventListener('click', function (){
     var checkbox = document.createElement('input'); 
     checkbox.type = "checkbox"; 
     checkbox.id = "id"; 
     var label = document.createElement('label'); 
     label.htmlFor = "id"; 
-    var item = document.getElementById('main_content__item').value;
+    var item = document.querySelector('.main_content__item').value;
     label.appendChild(document.createTextNode(item)); 
     todoList.appendChild(checkbox); 
     todoList.appendChild(label);
-    document.getElementById('main_content__item').value = '';
+    document.querySelector('.main_content__item').value = '';
   
-};
-var match = document.getElementById('id');
-if (match%3){
-    alert('yep');
+});
+function isChecked () {
+  var chbox=document.getElementById('id');
+    if (chbox.checked == true){
+      console.log("Флажок установлен");
+   }else{
+       console.log("Флажок не установлен");
+  }
 }
-//function isChecked () {
- //   var chbox;
- //   chbox=document.getElementById('id');
-//    if (chbox.checked){
- //       console.log("Флажок установлен");
- //   }else{
- //       console.log("Флажок не установлен");
- //   }
-//}
-//isChecked();
-document.getElementById('main_content__clear').onclick = function (){
-    document.getElementById('main_content__block').innerHTML = '';
-}
+isChecked();
+
+clear.addEventListener('click', function (){
+    document.querySelector('.main_content_block').innerHTML = '';
+});
