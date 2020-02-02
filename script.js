67a0ac47f20db746e82ef08ee9fa868e70da9fcd
Polina Raikina <rpaminsk1996@gmail.com>
2020-02-02 18:22:01 +0300

    no  crossing changes

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
-var add = document.querySelector('.main_content__add');
-var clear = document.querySelector('.main_content__clear');
-var todoList = document.querySelector(".main_content_block"); 
-add.addEventListener('click', function (){
-    var checkbox = document.createElement('input'); 
-    checkbox.type = "checkbox"; 
-    checkbox.id = "id"; 
-    var label = document.createElement('label'); 
-    label.htmlFor = "id"; 
-    var item = document.querySelector('.main_content__item').value;
-    label.appendChild(document.createTextNode(item)); 
-    todoList.appendChild(checkbox); 
-    todoList.appendChild(label);
-    document.querySelector('.main_content__item').value = '';
-  
-});
-function isChecked () {
-  var chbox=document.getElementById('id');
-    if (chbox.checked == true){
-      console.log("Флажок установлен");
-   }else{
-       console.log("Флажок не установлен");
-  }
-}
-isChecked();
-
-clear.addEventListener('click', function (){
+var todos = []
+  var todo = document.querySelector('.main_content__item')
+  var todoView = document.querySelector('.main_content_block')
+  var clear = document.querySelector('.main_content__clear');
+  document.querySelector('.main_content__add').addEventListener('click', function () {
+      var id = todos.length
+      todos.push(id)
+      var li = document.createElement('li')
+      var span = document.createElement('span')
+      span.innerText = todo.value
+      var checkbox = document.createElement('input')
+      checkbox.type = 'checkbox'
+      checkbox.todo = id
+      li.appendChild(checkbox)
+      li.appendChild(span)
+      todoView.appendChild(li)
+      todo.value = ''
+  });
+  clear.addEventListener('click', function (){
     document.querySelector('.main_content_block').innerHTML = '';
 });
