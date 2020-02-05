6e9295182e795f3d61dfdd676e32bb9d966f8a17
Polina <49366794+PolinaRaikina@users.noreply.github.com>
2020-02-05 19:12:24 +0300

    final result

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-var todos = []
-  var todo = document.querySelector('.main_content__item')
-  var todoView = document.querySelector('.main_content_block')
-  var clear = document.querySelector('.main_content__clear');
-  var add = document.querySelector('.main_content__add');
-  add.addEventListener('click', function () {
-      var id = todos.length
-      todos.push(id)
-      var li = document.createElement('li')
-      var span = document.createElement('span')
-      span.innerText = todo.value
-      var checkbox = document.createElement('input')
-      checkbox.type = 'checkbox'
-      checkbox.todo = id
-      li.appendChild(checkbox)
-      li.appendChild(span)
-      todoView.appendChild(li)
-      todo.value = ''
+var data = [];
+var task = document.querySelector('.main_content__item');
+var todoList = document.querySelector('.main_content_block');
+var clearBtn = document.querySelector('.main_content__clear');
+var addBtn = document.querySelector('.main_content__add');
+var form = document.querySelector('.main_content');
+function check(){
+  if (task.value.length > 1){
+    addBtn.disabled = false;
+  }
+};
+function createItem(e) {
+  var id = data.length;
+  var li = document.createElement('li');
+  var span = document.createElement('span');
+  var checkbox = document.createElement('input');
+  data.push(id);
+  span.innerText = task.value;
+  checkbox.type = 'checkbox';
+  checkbox.task = id;
+  li.appendChild(checkbox);
+  li.appendChild(span);
+  todoList.appendChild(li);
+  task.value = '';
+  addBtn.disabled = true;
+};
+function clearData(){
+  var emptyArr = document.querySelectorAll('.main_content_block > li');
+  emptyArr.forEach((e) => {
+    e.remove();
   });
-  clear.addEventListener('click', function (){
-    document.querySelector('.main_content_block').innerHTML = '';
+};
+clearBtn.addEventListener('click',clearData);
+addBtn.addEventListener('click', createItem);
+task.addEventListener("keydown", check, true);
+form.addEventListener('submit', function(e){
+  e.preventDefault();
 });
